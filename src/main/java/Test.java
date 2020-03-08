e8b6c9d5bb90af6ce989a8b9b86f806781dfcdf1
Alexander Bulgak <aleksandarbulgak@yandex.by>
2020-03-08 06:03:03 +0300

    add logger

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Test {
+
+    static final Logger rootLogger= LogManager.getRootLogger();
+    static final Logger classLogger= LogManager.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        rootLogger.info("!root log!");
+        classLogger.warn("!class log!");
+    }
+
 }
