212918c468bcedea7c619d13f368b2891880e110
Alexander Bulgak <aleksandarbulgak@yandex.by>
2020-03-18 06:10:57 +0300

    repair module-info, relocate logger to parent-pom

--- connectionPoolForDataBase/src/main/java/by/epam/signsControl/connectionPool/impl/ConnectionPool.java
+++ connectionPoolForDataBase/src/main/java/by/epam/signsControl/connectionPool/impl/ConnectionPool.java
@@ -7,6 +7,7 @@ import by.epam.signsControl.connectionPool.IConnectionPool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
---
+++ connectionPoolForDataBase/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module connectionPoolForDataBase {
+    requires java.sql;
+    requires org.apache.logging.log4j;
+
+
+    exports by.epam.signsControl.connectionPool;
+
+}
