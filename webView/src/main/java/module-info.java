c2c9e05ef995a6e2aed6282b5e5b395d05ee60e4
Alexander <aleksandarbulgak@yandex.by>
2020-05-21 02:55:07 +0200

    make webView work

---
+++ webView/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module webView {
+
+    requires signsControl;
+    requires rolesOrganisationsUsersController;
+
+    requires org.apache.logging.log4j;
+    requires java.sql;
+
+}
--- webView/src/main/webapp/index.jsp
+++ webView/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Here we go again!</h2>
 </body>
 </html>
