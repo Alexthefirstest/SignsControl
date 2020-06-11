3e79f0c73dbc448ccfeeddec79260a73c7d1cc8e
Alexander <aleksandarbulgak@yandex.by>
2020-06-11 18:57:46 +0200

    add citys on map

--- webView/src/main/webapp/WEB-INF/jsp/main.jsp
+++ webView/src/main/webapp/WEB-INF/jsp/main.jsp
@@ -5,10 +5,31 @@
 <head>
 
     <title>SignsControl</title>
-    <script src="https://api-maps.yandex.ru/2.1/?apikey=1c5fb506-d2a0-4d35-9f1f-6db2c0c54aa1&lang=ru_RU"
-                type="text/javascript"></script>
 
-<script src="${pageContext.request.contextPath}/js/map.js" type="text/javascript"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=1c5fb506-d2a0-4d35-9f1f-6db2c0c54aa1&lang=ru_RU"
+            type="text/javascript" charset="UTF-8"></script>
+
+    <script src="https://yandex.st/jquery/2.2.3/jquery.min.js" type="text/javascript"></script>
+
+    <script src="${pageContext.request.contextPath}/js/map.js" type="text/javascript" charset="UTF-8"></script>
+
+//создать css. карта, объекты
+    <style>
+        html, body, #map {
+            width: 100%;
+            height: 100%;
+            padding: 0;
+            margin: 0;
+        }
+
+        #my-listbox {
+            top: auto;
+            left: auto;
+        }
+    </style>
 
 </head>
 
@@ -18,7 +39,8 @@
 
 <p><a href="${pageContext.request.contextPath}/login">log in</a>
 
-<div id="map" style=" height: 500px"> </div>
+<%--<div id="map" style=" height: 500px"></div>--%>
+<div id="map" ></div>
 
 </body>
 
--- webView/src/main/webapp/js/map.js
+++ webView/src/main/webapp/js/map.js
@@ -4,53 +4,190 @@ ymaps.ready(init);
 
 function init() {
 
-    let myMap = new ymaps.Map("map", {center: [53.90, 27.56], zoom: 12,
-    controls: ['rulerControl','searchControl','typeSelector','zoomControl','geolocationControl','fullscreenControl']});
+    let myMap = new ymaps.Map("map", {
+        center: [53.90, 27.56], zoom: 12,
+        controls: ['rulerControl', 'searchControl', 'typeSelector', 'zoomControl', 'geolocationControl', 'fullscreenControl']
+    });
 
- // Обработка событий карты:
-                                  // - boundschange - изменение границ области показа;
-                                  // - type - изменение типа карты;
-      myMap.events.add(['boundschange', 'typechange'], setLocationHash);
+    //-----------------------------сохранение положения карты ---- начало
 
-        setMapStateByHash();
+    // Обработка событий карты:
+    // - boundschange - изменение границ области показа;
+    // - type - изменение типа карты;
+    myMap.events.add(['boundschange', 'typechange'], setLocationHash);
 
-          // Получение значение параметра name из адресной строки
-                    // браузера.
-                    function getParam (name, location) {
-                        location = location || window.location.hash;
-                        let res = location.match(new RegExp('[#&]' + name + '=([^&]*)', 'i'));
-                        return (res && res[1] ? res[1] : false);
-                    }
+    setMapStateByHash();
 
-                    // Передача параметров, описывающих состояние карты,
-                    // в адресную строку браузера.
-                    function setLocationHash () {
-                        let params = [
-                            'type=' + myMap.getType().split('#')[1],
-                            'center=' + myMap.getCenter(),
-                            'zoom=' + myMap.getZoom()
-                        ];
+    // Получение значение параметра name из адресной строки
+    // браузера.
+    function getParam(name, location) {
+        location = location || window.location.hash;
+        let res = location.match(new RegExp('[#&]' + name + '=([^&]*)', 'i'));
+        return (res && res[1] ? res[1] : false);
+    }
 
-                        window.location.hash = params.join('&');
-                    }
+    // Передача параметров, описывающих состояние карты,
+    // в адресную строку браузера.
+    function setLocationHash() {
+        let params = [
+            'type=' + myMap.getType().split('#')[1],
+            'center=' + myMap.getCenter(),
+            'zoom=' + myMap.getZoom()
+        ];
 
-                    // Установка состояния карты в соответствии с переданными в адресной строке
-                    // браузера параметрами.
-                    function setMapStateByHash () {
-                        let hashType = getParam('type'),
-                            hashCenter = getParam('center'),
-                            hashZoom = getParam('zoom');
+        window.location.hash = params.join('&');
+    }
 
-                        if (hashType) {
-                            myMap.setType('yandex#' + hashType);
-                        }
-                        if (hashCenter) {
-                            myMap.setCenter(hashCenter.split(','));
-                        }
-                        if (hashZoom) {
-                            myMap.setZoom(hashZoom);
-                        }
+    // Установка состояния карты в соответствии с переданными в адресной строке
+    // браузера параметрами.
+    function setMapStateByHash() {
+        let hashType = getParam('type'),
+            hashCenter = getParam('center'),
+            hashZoom = getParam('zoom');
 
-                    }
+        if (hashType) {
+            myMap.setType('yandex#' + hashType);
+        }
+        if (hashCenter) {
+            myMap.setCenter(hashCenter.split(','));
+        }
+        if (hashZoom) {
+            myMap.setZoom(hashZoom);
+        }
+
+    }
+
+    // ----------------------- сохранение положения карты - конец
+
+    // ----------------------- список городов - начало
+
+     let ListBoxLayout = ymaps.templateLayoutFactory.createClass(
+        "<button id='my-listbox-header' class='btn btn-success dropdown-toggle' data-toggle='dropdown'>" +
+        "{{data.title}} <span class='caret'></span>" +
+        "</button>" +
+        // Этот элемент будет служить контейнером для элементов списка.
+        // В зависимости от того, свернут или развернут список, этот контейнер будет
+        // скрываться или показываться вместе с дочерними элементами.
+        "<ul id='my-listbox'" +
+        " class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu'" +
+        " style='display: {% if state.expanded %}block{% else %}none{% endif %};'></ul>", {
+
+                build: function () {
+                    // Вызываем метод build родительского класса перед выполнением
+                    // дополнительных действий.
+                    ListBoxLayout.superclass.build.call(this);
+
+                    this.childContainerElement = $('#my-listbox').get(0);
+                    // Генерируем специальное событие, оповещающее элемент управления
+                    // о смене контейнера дочерних элементов.
+                    this.events.fire('childcontainerchange', {
+                        newChildContainerElement: this.childContainerElement,
+                        oldChildContainerElement: null
+                    });
+                },
+
+                // Переопределяем интерфейсный метод, возвращающий ссылку на
+                // контейнер дочерних элементов.
+                getChildContainerElement: function () {
+                    return this.childContainerElement;
+                },
+
+                clear: function () {
+                    // Заставим элемент управления перед очисткой макета
+                    // откреплять дочерние элементы от родительского.
+                    // Это защитит нас от неожиданных ошибок,
+                    // связанных с уничтожением dom-элементов в ранних версиях ie.
+                    this.events.fire('childcontainerchange', {
+                        newChildContainerElement: null,
+                        oldChildContainerElement: this.childContainerElement
+                    });
+                    this.childContainerElement = null;
+                    // Вызываем метод clear родительского класса после выполнения
+                    // дополнительных действий.
+                    ListBoxLayout.superclass.clear.call(this);
+                }
+            }),
+
+        // Также создадим макет для отдельного элемента списка.
+        ListBoxItemLayout = ymaps.templateLayoutFactory.createClass(
+            "<li><a>{{data.content}}</a></li>"
+        ),
+
+        // Создадим  пункта выпадающего списка
+        listBoxItems = [
+            new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Минск',
+                    center: [53.90, 27.56],
+                    zoom: 12
+                }
+            }),     new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Брест',
+                    center: [52.09776408529675,23.69610453788942],
+                    zoom: 12
+                }
+            }),   new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Витебск',
+                    center: [55.1926727351534,30.206306926835314],
+                    zoom: 12
+                }
+            }),   new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Гомель',
+                    center: [52.42510947995823,31.009052120149114],
+                    zoom: 12
+                }
+            }),   new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Гродно',
+                    center: [53.678083189470364,23.830716072310775],
+                    zoom: 12
+                }
+            }),
+            new ymaps.control.ListBoxItem({
+                data: {
+                    content: 'Могилёв',
+                    center: [53.898049170273595,30.332562737166853],
+                    zoom: 12
+                }
+            })
+        ],
+
+        // Теперь создадим список.
+        listBox = new ymaps.control.ListBox({
+            items: listBoxItems,
+            data: {
+                title: 'Областные центры'
+            },
+            options: {
+                // С помощью опций можно задать как макет непосредственно для списка,
+                layout: ListBoxLayout,
+                // так и макет для дочерних элементов списка. Для задания опций дочерних
+                // элементов через родительский элемент необходимо добавлять префикс
+                // 'item' к названиям опций.
+                itemLayout: ListBoxItemLayout
+            }
+        });
+
+    listBox.events.add('click', function (e) {
+        // Получаем ссылку на объект, по которому кликнули.
+        // События элементов списка пропагируются
+        // и их можно слушать на родительском элементе.
+        let item = e.get('target');
+        // Клик на заголовке выпадающего списка обрабатывать не надо.
+        if (item != listBox) {
+            myMap.setCenter(
+                item.data.get('center'),
+                item.data.get('zoom')
+            );
+        }
+    });
+
+    myMap.controls.add(listBox, {float: 'left'});
+
+    // ----------------------- список городов - конец
+
 
 }
