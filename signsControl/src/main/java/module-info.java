77126fe159500d55aa5a77f386153fb414740d88
Alexander <aleksandarbulgak@yandex.by>
2020-06-17 14:07:46 +0200

    add exports to pom

--- signsControl/src/main/java/module-info.java
+++ signsControl/src/main/java/module-info.java
@@ -5,4 +5,9 @@ module signsControl {
     requires org.apache.logging.log4j;
     requires java.sql;
 
+    exports by.epam.signsControl.bean;
+    exports by.epam.signsControl.service;
+    exports by.epam.signsControl.service.factory;
+    exports by.epam.signsControl.service.exceptions;
+
 }
