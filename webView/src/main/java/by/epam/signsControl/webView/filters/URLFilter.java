14fbe20ed9d249ebb8c1f77df89a1f279ae6ba51
Alexander <aleksandarbulgak@yandex.by>
2020-06-17 14:03:43 +0200

    repair controlelr, add point on map

--- webView/src/main/java/by/epam/signsControl/webView/filters/URLFilter.java
+++ webView/src/main/java/by/epam/signsControl/webView/filters/URLFilter.java
@@ -1,45 +1,45 @@
-//package by.epam.signsControl.webView.filters;
-//
-//
-//import by.epam.signsControl.webView.servlets.CommandController;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//
-//import javax.servlet.Filter;
-//import javax.servlet.FilterChain;
-//import javax.servlet.FilterConfig;
-//import javax.servlet.ServletException;
-//import javax.servlet.ServletRequest;
-//import javax.servlet.ServletResponse;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import java.io.IOException;
-//
-//@WebFilter(urlPatterns = "/*")
-//public class URLFilter implements Filter {
-//
-//    private static final String SERVLET_PATH = "/app";
-//    public static final String REQUIRED_URI = "uri";
-//    private static final Logger logger = LogManager.getLogger(Filter.class);
-//
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//
-//        logger.info("filter");
-//        request.setAttribute(REQUIRED_URI, ((HttpServletRequest)request).getRequestURI());
-//        request.getRequestDispatcher(SERVLET_PATH).forward(request, response);
-//        logger.info("filter middle");
-//
-//        logger.info("end of filter");
-//    }
-//
-//    @Override
-//    public void destroy() {
-//
-//    }
-//}
+package by.epam.signsControl.webView.filters;
+
+
+import by.epam.signsControl.webView.servlets.CommandController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class URLFilter implements Filter {
+
+    private static final String SERVLET_PATH = "/app";
+    public static final String REQUIRED_URI = "uri";
+    private static final Logger logger = LogManager.getLogger(Filter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        logger.info("filter");
+        request.setAttribute(REQUIRED_URI, ((HttpServletRequest)request).getRequestURI());
+        request.getRequestDispatcher(SERVLET_PATH).forward(request, response);
+        logger.info("filter middle");
+
+        logger.info("end of filter");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- webView/src/main/java/by/epam/signsControl/webView/servlets/CommandController.java
+++ webView/src/main/java/by/epam/signsControl/webView/servlets/CommandController.java
@@ -1,51 +1,51 @@
-//package by.epam.signsControl.webView.servlets;
-//
-//import by.epam.signsControl.webView.filters.URLFilter;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
-//
-//@WebServlet(name = "Main Servlet", urlPatterns = "/app")
-//
-//public class CommandController extends HttpServlet {
-//
-//    private static final Logger logger = LogManager.getLogger(CommandController.class);
-////    private static final CommandExecutor commandExecutor = new CommandExecutor();
-//
-//    private String getCommandFromURI(String uri, String contextPath) {
-//
-//        Pattern pattern = Pattern.compile(contextPath + "/([^/]+)");
-//        Matcher matcher = pattern.matcher(uri);
-//
-//        return matcher.find() ? matcher.group(1) : "mainPage";
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
-//        String contextPath = req.getContextPath();
-//
-//        logger.info("inside servlet");
-//
-////        commandExecutor.execute(url); default - main jsp
-//        logger.warn("mm1");
-//
-//        req.setAttribute("myCommand",
-//                getCommandFromURI(req.getAttribute(URLFilter.REQUIRED_URI).toString(), contextPath));
-//
-//        logger.warn(req.getRequestURI());
-//
-//        //eto bydet v commandah мб сделать переход на интекс.жсп!!!!!!!!!!!!!!!!!
-//        req.getRequestDispatcher("/WEB-INF/jsp/main.jsp").forward(req, resp);
-//        logger.warn("poch ya tut, a?");
-//    }
-//
-//}
+package by.epam.signsControl.webView.servlets;
+
+import by.epam.signsControl.webView.filters.URLFilter;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet(name = "Main Servlet", urlPatterns = "/app")
+
+public class CommandController extends HttpServlet {
+
+    private static final Logger logger = LogManager.getLogger(CommandController.class);
+//    private static final CommandExecutor commandExecutor = new CommandExecutor();
+
+    private String getCommandFromURI(HttpServletRequest request) {
+        logger.warn("g1");
+        Pattern pattern = Pattern.compile(request.getContextPath() + "/([^/]+)");
+        logger.warn("g2");
+        Matcher matcher = pattern.matcher(request.getAttribute(URLFilter.REQUIRED_URI).toString());
+        logger.warn("g3");
+        return matcher.find() ? matcher.group(1) : "mainPage";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        logger.info("inside servlet");
+
+//        commandExecutor.execute(url); default - main jsp
+        logger.warn("mm1 " + req.getAttribute(URLFilter.REQUIRED_URI));
+
+        req.setAttribute("myCommand", getCommandFromURI(req));
+
+
+        //eto bydet v commandah мб, мб сделать переход на интекс.жсп!!!!!!!!!!!!!!!!!
+        //   req.getRequestDispatcher(req.getContextPath()+"/WEB-INF/jsp/main.jsp").forward(req, resp);
+        req.getRequestDispatcher("/").forward(req, resp);
+        logger.warn("poch ya tut, a?");
+    }
+
+}
